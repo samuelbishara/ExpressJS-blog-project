2f4572798bf5bfcb49d304ba454f5ca705feb4c1
Sanuel Bishara <sambishara99@gmail.com>
2020-12-05 13:43:16 -0500

    Created dynamic posts template, dynamic http routing and cleaned up home page design

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('Express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const lodash = require('lodash');
 
 const homeStartingContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nunc tellus, imperdiet ac sapien sit amet, porta aliquet orci. Nunc ac pretium quam, ut luctus purus. Aliquam turpis magna, mollis varius sollicitudin quis, suscipit quis nisi. Integer neque ipsum, porttitor eleifend eros lacinia, commodo interdum lectus. Cras vitae laoreet quam. Nullam fermentum elit in pharetra gravida. In eu elit congue, volutpat urna vel, tincidunt sem. Quisque pretium augue sit amet ante imperdiet sagittis. Sed iaculis neque quam, et suscipit nunc lobortis eu. Maecenas pretium finibus ligula, a lobortis elit vulputate et. Donec nec quam et nibh vestibulum egestas.'
 const aboutStartingContent = 'Aliquam erat volutpat. Vestibulum at faucibus nisl. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam non maximus quam, et vehicula metus. Curabitur at tristique ante, a ultricies nisl. Quisque feugiat porttitor risus, quis tincidunt ligula mollis at. Nam pellentesque sed elit quis tincidunt. In accumsan egestas sagittis. In gravida lacus orci, vitae congue est ultricies ac. Fusce lacus risus, eleifend vel venenatis at, congue nec velit. Vivamus tempus vulputate tristique. Proin sed accumsan tortor. Cras lacinia elementum tellus, in mollis nibh egestas vel. Cras porta magna ac est molestie, non vehicula enim lacinia.'
@@ -31,6 +32,17 @@ app.get('/contact', (req, res) => {
 app.get('/compose', (req, res) => {
     res.render('compose')
 });
+app.get('/posts/:selectedPost', (req, res) => {
+    const selectedTitle = lodash.lowerCase(req.params.selectedPost);
+    posts.forEach((post) => {
+        const storedTitle = lodash.lowerCase(post.title);
+        if (storedTitle === selectedTitle){
+            res.render('post', {
+                postTitle: post.title, 
+                postBody: post.body})
+        }
+    })
+});
 
 app.post('/compose', (req, res) => {
     const postTitle = req.body.postTitle;
@@ -39,7 +51,7 @@ app.post('/compose', (req, res) => {
         title: postTitle,
         body: postBody
     }
-    posts.push(post)
+    posts.unshift(post)
     res.redirect('/');
 });
 
