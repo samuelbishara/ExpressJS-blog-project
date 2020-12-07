2aa20bbd9fd93e48f620d8a83df3ed36d0bc5587
Sanuel Bishara <sambishara99@gmail.com>
2020-12-07 13:38:08 -0500

    Minor Bug Fixed

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const ejs = require('ejs');
 const lodash = require('lodash');
 const mongoose = require('mongoose');
 
+const aboutStartingContent = 'Aliquam erat volutpat. Vestibulum at faucibus nisl. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam non maximus quam, et vehicula metus. Curabitur at tristique ante, a ultricies nisl. Quisque feugiat porttitor risus, quis tincidunt ligula mollis at. Nam pellentesque sed elit quis tincidunt. In accumsan egestas sagittis. In gravida lacus orci, vitae congue est ultricies ac. Fusce lacus risus, eleifend vel venenatis at, congue nec velit. Vivamus tempus vulputate tristique. Proin sed accumsan tortor. Cras lacinia elementum tellus, in mollis nibh egestas vel. Cras porta magna ac est molestie, non vehicula enim lacinia.'
+const contactStartingContent = 'Etiam ipsum mauris, tincidunt in lobortis eget, tristique fringilla urna. Praesent sem leo, convallis id congue vel, posuere at nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu erat vitae odio sodales imperdiet. Morbi id ultrices elit. Aenean sit amet sollicitudin elit. Fusce vestibulum posuere lectus, vitae lacinia nibh facilisis ac. Aenean ut erat sit amet erat lacinia tincidunt. Praesent in rutrum sapien. Sed elit orci, rutrum id dignissim viverra, pellentesque vitae metus. Integer a sagittis lacus.'
+
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -23,6 +27,7 @@ const postSchema = {
     content: String
 }
 const Post = mongoose.model("Post", postSchema);
+
 
 app.get('/', (req, res) => {
     Post.find({}, (e, foundItems) => {
